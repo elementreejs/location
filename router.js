85379d2c19d529244eb1a7c6b88abd058d699cb7
Mark Stahl <mjstahl@gmail.com>
2019-07-20 16:58:21 -0400

    Refactor to get away from classes. It is a function now

--- router.js
+++ router.js
@@ -1,65 +1,59 @@
 const FIND_ROUTE_VAR = new RegExp('(:\\w+)', 'g')
 const REPL_ROUTE_VAR = '(\\w+)'
 
-export default class Router {
-  get path () {
-    const found = Object.keys(this._pathMatches).find(match => {
-      return new RegExp(match, 'g').test(this._route)
-    })
-    const matched = this._pathMatches[found]
-    if (!matched) return {}
+export default function routeData (state) {
+  const pathMatches = state.routes.reduce((matches, path) => {
+    const vars = (path.match(FIND_ROUTE_VAR) || []).map(v => v.slice(1))
+    const regex = path.replace(FIND_ROUTE_VAR, REPL_ROUTE_VAR)
+    matches[regex] = { path, regex, vars }
+    return matches
+  }, {})
 
-    const values = this._route.match(matched.regex).slice(1)
-    return matched.vars.reduce((v, p, i) => {
-      return Object.defineProperty(v, p, {
-        get () { return values[i] },
-        set: this._pathSetter(matched, p)
+  return {
+    get path () {
+      const found = Object.keys(pathMatches).find(match => {
+        return new RegExp(match, 'g').test(state.route)
       })
-    }, {})
-  }
+      const matched = pathMatches[found]
+      if (!matched) return {}
 
-  get route () {
-    return this._route
-  }
-
-  set route (current) {
-    this._route = current
-    if (!this._pathMatches) {
-      this._pathMatches = this.routes.reduce((matches, path) => {
-        const vars = (path.match(FIND_ROUTE_VAR) || []).map(v => v.slice(1))
-        const regex = path.replace(FIND_ROUTE_VAR, REPL_ROUTE_VAR)
-        matches[regex] = { path, regex, vars }
-        return matches
+      const values = state.route.match(matched.regex).slice(1)
+      return matched.vars.reduce((v, p, i) => {
+        return Object.defineProperty(v, p, {
+          get () { return values[i] },
+          set: this._pathSetter(matched, p)
+        })
       }, {})
-    }
-  }
+    },
 
-  get query () {
-    const search = document.location.search
-    if (!search) return search
-    const pairs = search.slice(1).split('&')
-    return pairs.reduce((q, p) => {
-      const [key, value] = p.split('=')
-      return Object.defineProperty(q, key, {
-        get () { return value },
-        set: this._querySetter(key, value)
-      })
-    }, {})
-  }
-
-  _pathSetter (match, prop) {
-    return value => {
-      const path = match.path.replace(new RegExp(`:${prop}`, 'g'), value)
-      this.route = path + document.location.search + document.location.hash
-    }
-  }
-
-  _querySetter (prop, prevValue) {
-    return value => {
+    get query () {
       const search = document.location.search
-        .replace(new RegExp(`${prop}=${prevValue}`, 'g'), `${prop}=${value}`)
-      this.route =
-        document.location.pathname + search + document.location.hash
+      if (!search) return search
+
+      const pairs = search.slice(1).split('&')
+      return pairs.reduce((q, p) => {
+        const [key, value] = p.split('=')
+        return Object.defineProperty(q, key, {
+          get () { return value },
+          set: this._querySetter(key, value)
+        })
+      }, {})
+    },
+
+    _pathSetter: function (match, prop) {
+      return value => {
+        const path = match.path.replace(new RegExp(`:${prop}`, 'g'), value)
+        state.route = path + document.location.search + document.location.hash
+      }
+    },
+
+    _querySetter: function (prop, prevValue) {
+      return value => {
+        const search = document.location.search
+          .replace(new RegExp(`${prop}=${prevValue}`, 'g'), `${prop}=${value}`)
+        state.route =
+          document.location.pathname + search + document.location.hash
+      }
     }
   }
 }
